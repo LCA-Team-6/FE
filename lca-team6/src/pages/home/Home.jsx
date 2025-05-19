0af742620a8e05e31939807e90efd00abfec61b6
minju26 <mjalswn26@gmail.com>
2025-05-19 23:34:15 +0900

    Feat: 홈 페이지 생성

---
+++ lca-team6/src/pages/home/Home.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Calendar from './Calendar'
+
+const diaryDates = [
+  new Date(2025, 4, 10),
+  new Date(2025, 4, 12),
+];
+
+const Home = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const hasDiary = (date) => {
+    return diaryDates.some(
+      (d) => d.toDateString() === date.toDateString()
+    );
+  };
+
+  return (
+    <div>
+      <Calendar
+        selectedDate={selectedDate}
+        onDateSelect={setSelectedDate}
+      />
+
+      <div >
+        {selectedDate ? (
+          hasDiary(selectedDate) ? (
+            <p><strong>{selectedDate.toDateString()}</strong>: 이 날 작성한 일기 내용</p>
+          ) : (
+            <p><strong>{selectedDate.toDateString()}</strong>: 작성된 일기가 없습니다.</p>
+          )
+        ) : (
+          <p>날짜를 선택해 주세요.</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
