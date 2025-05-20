cdb1b20e90ac432045af977d34e4a6ff48b78b9c
131c2 <bg3189@naver.com>
2025-05-20 15:15:51 +0900

    Feat: 피드백 받기 기능

--- lca-team6/src/routes/AppRoutes.jsx
+++ lca-team6/src/routes/AppRoutes.jsx
@@ -1,8 +1,11 @@
 import { Route } from "react-router-dom";
 import Example from "../pages/example/Example";
+import Write from "../pages/write/Write";
 
 export default function AppRoutes() {
   return [
     <Route key='example' path='/example' element={<Example />} />,
+    <Route key='write' path='/write' element={<Write />} />,
+
   ];
 }
