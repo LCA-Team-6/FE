15788e852928ca2c1acc0a786bb67da6463839ac
minju26 <mjalswn26@gmail.com>
2025-05-21 15:40:15 +0900

    Fix:  불필요한 코드 제거

--- lca-team6/src/routes/AppRoutes.jsx
+++ lca-team6/src/routes/AppRoutes.jsx
@@ -6,17 +6,6 @@ import Register from "../pages/login/Register";
 import Write from "../pages/write/Write";
 import Prompts from "../pages/prompts/Prompts";
 
-export default function AppRoutes() {
-  return [
-    <Route key='example' path='/example' element={<Example />} />,
-    <Route key='home' path='/' element={<Home />} />,
-    <Route key='login' path='/login' element={<Login />} />,
-    <Route key='register' path='/register' element={<Register />} />,
-    <Route key='write' path='/write' element={<Write />} />,
-    <Route key='promtps' path='/prompts' element={<Prompts />} />,
-
-import NavBarShow from "../nav/NavBarShow";
-
 const AppRoutes = [
   // 네비게이션 버튼 보이는 페이지
   <Route element={<NavBarShow />} key="layout">
