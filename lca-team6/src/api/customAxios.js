7317926638dac1fe76db130fa4f69a18c2244af9
minju26 <mjalswn26@gmail.com>
2025-05-21 11:38:32 +0900

    Fix: 로컬 스토리지에 저장할 토큰 이름 변경

--- lca-team6/src/api/customAxios.js
+++ lca-team6/src/api/customAxios.js
@@ -10,7 +10,7 @@ const customAxios = axios.create({
 // 요청 인터셉터
 customAxios.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('accessToken');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
