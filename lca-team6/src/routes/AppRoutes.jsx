cd5d41d1d2ee06f7cd128bef5171706978d12468
131c2 <bg3189@naver.com>
2025-05-21 10:55:13 +0900

    Refactor: 네비게이션 버튼 보이는 페이지 구별, AppRoutes.jsx 수정

--- lca-team6/src/routes/AppRoutes.jsx
+++ lca-team6/src/routes/AppRoutes.jsx
@@ -4,15 +4,18 @@ import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import Register from "../pages/login/Register";
 import Write from "../pages/write/Write";
+import NavBarShow from "../nav/NavBarShow";
 
-export default function AppRoutes() {
-  return[
-    
-    <Route key='example' path='/example' element={<Example />} />,
-    <Route key='home' path='/' element={<Home />} />,
-    <Route key='login' path='/login' element={<Login />} />,
-    <Route key='register' path='/register' element={<Register />} />,
-    <Route key='write' path='/write' element={<Write />} />
-    
-  ];
-}
+const AppRoutes = [
+  // 네비게이션 버튼 보이는 페이지
+  <Route element={<NavBarShow />} key="layout">
+    <Route path='/' element={<Home />} />
+    <Route path='/example' element={<Example />} />
+    <Route path='/write' element={<Write />} />
+  </Route>,
+  // 네비게이션 버튼 안보이는 페이지
+  <Route path='/login' element={<Login />} key="login" />,
+  <Route path='/register' element={<Register />} key="register" />,
+];
+
+export default AppRoutes;
