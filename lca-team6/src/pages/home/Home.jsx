5a20031bb241f9f4bd97c395f6c90e84996e3f30
minju26 <mjalswn26@gmail.com>
2025-05-21 15:45:56 +0900

    Refactor: axios 임포트 제거

--- lca-team6/src/pages/home/Home.jsx
+++ lca-team6/src/pages/home/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Calendar from './calendar'
 import './Home.css'
-import axios from 'axios';
 import customAxios from '../../api/customAxios';
 
 const Home = () => {
