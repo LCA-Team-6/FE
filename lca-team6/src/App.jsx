e3c27addc3652a2b7affa15e37b08205e844c8d8
131c2 <bg3189@naver.com>
2025-05-21 10:53:22 +0900

    Refactor: App.jsx 에서 AppRoutes코드 수정

--- lca-team6/src/App.jsx
+++ lca-team6/src/App.jsx
@@ -2,15 +2,13 @@ import { BrowserRouter, Routes } from "react-router-dom";
 import { Global } from "@emotion/react";
 import { GlobalStyle } from "./styles/globalStyle";
 import AppRoutes from "./routes/AppRoutes";
-import NavBar from "./nav/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
       <Global styles={GlobalStyle} />
-      <NavBar />
       <Routes>
-        {AppRoutes()}
+        {AppRoutes}
       </Routes>
     </BrowserRouter>
   );
