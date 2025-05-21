3d2d83fa88b5c395e78cb85fe3d06685de1bca3d
mz <ieecno22@gmail.com>
2025-05-21 09:47:33 +0900

    Feat: 회원가입 화면 추가

--- lca-team6/src/routes/AppRoutes.jsx
+++ lca-team6/src/routes/AppRoutes.jsx
@@ -2,14 +2,14 @@ import { Route } from "react-router-dom";
 import Example from "../pages/example/Example";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
-// import Register from "../pages/login/Register";
+import Register from "../pages/login/Register";
 
 export default function AppRoutes() {
   return [
     <Route key='example' path='/example' element={<Example />} />,
     <Route key='home' path='/' element={<Home />} />,
     <Route key='login' path='/login' element={<Login />} />,
-    // <Route key='register' path='/register' element={<Register />} />,
+    <Route key='register' path='/register' element={<Register />} />,
     
   ];
 }
