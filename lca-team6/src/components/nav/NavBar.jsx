b0452e22034f7b749ec3db1fa878277e2a92f8d8
minju26 <mjalswn26@gmail.com>
2025-05-22 14:26:59 +0900

    Feat: NavBar에 항목 추가

--- lca-team6/src/components/nav/NavBar.jsx
+++ lca-team6/src/components/nav/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useLocation, useNavigate} from "react-router-dom";
 import "./NavBar.css";
-import Logo from "../Logo";
+import Logo from "../logo/Logo";
 
 const NavBar = () =>{
     const location = useLocation();
@@ -10,10 +10,9 @@ const NavBar = () =>{
     const routes = [
         { name: "글쓰기", path:"/write" },
         { name: "홈", path:"/" },
+        { name: "마이 페이지", path:"/user" },
+        { name: "개인 설정", path:"/prompts" },
         { name: "로그아웃", path:"/login" }
-
-        // { name: "개인 설정", path:"/" },
-        // { name: "마이페이지", path:"/" },
     ];
     return(
         <div className="nav-bar-container">
